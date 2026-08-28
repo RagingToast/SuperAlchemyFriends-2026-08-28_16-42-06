1a53549d7a216318c3c1d38fda1f1f65489f2bd6
RagingToast <wiebewilbers8@gmail.com>
2026-08-28 22:43:54 +0200

    Added throw mechanic

--- Assets/WiebeFolder/Scripts/Actions.cs
+++ Assets/WiebeFolder/Scripts/Actions.cs
@@ -3,8 +3,27 @@ using UnityEngine.InputSystem;
 
 public class Actions : MonoBehaviour
 {
+    [SerializeField] private GameObject potion;
+    [SerializeField] private Transform fpCamera;
+    [SerializeField] private Transform potionHold;
+    
+    private Rigidbody _rb;
+    private float _throwForce = 3f;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    
+    
     public void OnThrow(InputAction.CallbackContext context)
     {
         Debug.Log("OnThrow");
+        _rb.isKinematic = false;
+        _rb.useGravity = true;
+
+        _rb.linearVelocity = fpCamera.forward * _throwForce + Vector3.up * 5f;
+        
+        Instantiate(potion, potionHold.position, fpCamera.rotation);
     }
 }
